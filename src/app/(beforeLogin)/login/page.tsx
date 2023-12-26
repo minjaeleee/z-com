a48174d780f3751d0f45b76a786608617408ab83
odin@snaps.com <dlalswo4602@naver.com>
2023-12-26 22:59:28 +0900

    Main 공통 컴포넌트 분리

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,16 +1,7 @@
-import Main from "@/app/(beforeLogin)/_component/Main";
-import {auth} from "@/auth";
-import {redirect} from "next/navigation";
 import RedirectToLogin from "@/app/(beforeLogin)/login/_component/RedirectToLogin";
+import Main from "@/app/(beforeLogin)/_component/Main";
 
 export default async function Login() {
-  const session = await auth();
-
-  if (session?.user) {
-    redirect('/home');
-    return null;
-  }
-
   return (
     <>
       <RedirectToLogin />
