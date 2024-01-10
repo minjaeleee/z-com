7198e9a7b19f6a0cc0af92b3e2d05e2008395ed7
odin@snaps.com <dlalswo4602@naver.com>
2024-01-10 20:11:33 +0900

    react-query fetch 옵션

--- src/app/(afterLogin)/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/_component/PostRecommends.tsx
@@ -6,8 +6,12 @@ import Post from "@/app/(afterLogin)/_component/Post";
 import { getPostRecommends } from "../_lib/getPostRecommends";
 
 export default function PostRecommends() {
-  const { data } = useQuery<IPost[]>({ queryKey: ['posts', 'recommends'], queryFn: getPostRecommends})
-  console.log("data",data)
+  const { data } = useQuery<IPost[]>({ 
+    queryKey: ['posts', 'recommends'], 
+    queryFn: getPostRecommends,
+    staleTime: 60 * 1000, // fresh -> stale 변경되는 시간
+    gcTime: 300 * 1000 // inactive일 때, 메모리를 비우는 시간. 보통 staleTime 보다 길어야 한다.
+  })
 
   return data?.map((post) => (
     <Post key={post.postId} post={post}/>
