b657428c0c58fe6fea8eca3cdf0fce00f5b3abf6
odin@snaps.com <dlalswo4602@naver.com>
2024-01-14 14:57:18 +0900

    react-intersection-observer를 통한 무한 스크롤 기능 구현

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { InfiniteData, useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 import { Post as IPost } from "@/model/Post";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { getPostRecommends } from "../../_lib/getPostRecommends";
+import { useInView } from "react-intersection-observer";
 
 export default function PostRecommends() {
-  const { data } = useInfiniteQuery<IPost[], Object, InfiniteData<IPost[]>, [_1: string, _2:string], number>({ 
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<IPost[], Object, InfiniteData<IPost[]>, [_1: string, _2:string], number>({ 
     queryKey: ['posts', 'recommends'], 
     queryFn: getPostRecommends,
     initialPageParam: 0,
@@ -17,11 +18,29 @@ export default function PostRecommends() {
     gcTime: 300 * 1000 // inactive일 때, 메모리를 비우는 시간. 보통 staleTime 보다 길어야 한다.
   })
 
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 0
+  })
+
+  useEffect(()=>{
+    if(inView) {
+     !isFetching && hasNextPage && fetchNextPage()
+    }
+  },[inView, hasNextPage, fetchNextPage, isFetching])
+
   return data?.pages.map((page, i) => (
-    <Fragment key={i}>
+    <>
       {
-        page.map((post) => <Post key={post.postId} post={post}/>)
+        data?.pages.map((page,i) => (
+          <Fragment key={i}>
+            {
+              page.map((post) => <Post key={post.postId} post={post}/>)
+            }
+          </Fragment>
+        ))
       }
-    </Fragment>
+      <div ref={ref} style={{ height: 50 }} />
+    </>
   ))
 }
