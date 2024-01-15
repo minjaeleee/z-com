5e3795d48e0b85c1d77c8d35798cf0d44db19493
odin@snaps.com <dlalswo4602@naver.com>
2024-01-16 06:50:35 +0900

    react-query에서 useSuspense를 사용하여 loading 처리 - 매번 isPending으로 커스터마이징 처리해줄 필요 없이, useSuspense를 사용하여 Suspense를 인식하여 pending 컴포넌트 처리를 하게 끔 처리

--- src/app/(afterLogin)/home/_component/FollowingPosts.tsx
+++ src/app/(afterLogin)/home/_component/FollowingPosts.tsx
@@ -1,34 +1,18 @@
 'use client'
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { Post as IPost } from "@/model/Post";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { getFollowingPosts } from "../_lib/getFollowingPosts";
 
-import styles from '@/app/(afterLogin)/home/home.module.css';  
-
-
 export default function FollowingPosts() {
-  const { data, isPending } = useQuery<IPost[]>({ 
+  const { data } = useSuspenseQuery<IPost[]>({ 
     queryKey: ['posts', 'followings'], 
     queryFn: getFollowingPosts,
     staleTime: 60 * 1000, // fresh -> stale 변경되는 시간
     gcTime: 300 * 1000 // inactive일 때, 메모리를 비우는 시간. 보통 staleTime 보다 길어야 한다.
   })
-
-  if (isPending) {
-    return (
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <svg className={styles.loader} height="100%" viewBox="0 0 32 32" width={40}>
-          <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4"
-                  style={{stroke: 'rgb(29, 155, 240)', opacity: 0.2}}></circle>
-          <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4"
-                  style={{stroke: 'rgb(29, 155, 240)', strokeDasharray: 80, strokeDashoffset: 60}}></circle>
-        </svg>
-      </div>
-    )
-  }
 
   return data?.map((post) => (
     <Post key={post.postId} post={post}/>
--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,17 +1,15 @@
 'use client'
 
 import { Fragment, useEffect } from "react";
-import { InfiniteData, useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { InfiniteData, useInfiniteQuery, useQuery, useSuspenseInfiniteQuery } from "@tanstack/react-query";
 
 import { Post as IPost } from "@/model/Post";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { getPostRecommends } from "../../_lib/getPostRecommends";
 import { useInView } from "react-intersection-observer";
 
-import styles from '@/app/(afterLogin)/home/home.module.css';
-
 export default function PostRecommends() {
-  const { data, fetchNextPage, hasNextPage, isFetching, isPending } = useInfiniteQuery<IPost[], Object, InfiniteData<IPost[]>, [_1: string, _2:string], number>({ 
+  const { data, fetchNextPage, hasNextPage, isFetching } = useSuspenseInfiniteQuery<IPost[], Object, InfiniteData<IPost[]>, [_1: string, _2:string], number>({ 
     queryKey: ['posts', 'recommends'], 
     queryFn: getPostRecommends,
     initialPageParam: 0,
@@ -30,19 +28,6 @@ export default function PostRecommends() {
      !isFetching && hasNextPage && fetchNextPage()
     }
   },[inView, hasNextPage, fetchNextPage, isFetching])
-
-  if (isPending) {
-    return (
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <svg className={styles.loader} height="100%" viewBox="0 0 32 32" width={40}>
-          <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4"
-                  style={{stroke: 'rgb(29, 155, 240)', opacity: 0.2}}></circle>
-          <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4"
-                  style={{stroke: 'rgb(29, 155, 240)', strokeDasharray: 80, strokeDashoffset: 60}}></circle>
-        </svg>
-      </div>
-    )
-  }
 
   return data?.pages.map((page, i) => (
     <>
